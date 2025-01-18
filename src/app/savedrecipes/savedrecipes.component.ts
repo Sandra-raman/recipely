8399a080a854ad7c809a470a53df7b166386907e
Sandraraman21 <sandraraman2023@gmail.com>
2025-01-18 10:40:46 +0530

    18/jan

---
+++ src/app/savedrecipes/savedrecipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-savedrecipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './savedrecipes.component.html',
+  styleUrl: './savedrecipes.component.css'
+})
+export class SavedrecipesComponent {
+
+}
---
+++ src/app/viewrecipes/viewrecipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewrecipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './viewrecipes.component.html',
+  styleUrl: './viewrecipes.component.css'
+})
+export class ViewrecipesComponent {
+
+}
